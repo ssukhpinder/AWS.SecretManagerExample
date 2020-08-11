256a118c07889c80e6d50f6bf91a0c74f4c93ba4
Sukhpinder Singh <sukhpinder.singh@sourcefuse.com>
2020-08-11 14:35:37 +0530

    Initial commit

---
+++ AWS.SecretManagerExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Extensions.Caching;
+using Amazon.SecretsManager.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWS.SecretManagerExample
+{
+    class Program
+    {
+        public static SecretsManagerCache cache;
+        public static string GetCacheData(string secretName)
+        {
+            return cache.GetSecretString(secretName).Result;
+        }
+        public static string GetSecret(string secretName)
+        {
+            var config = new AmazonSecretsManagerConfig { RegionEndpoint = RegionEndpoint.USEast2 };            
+            IAmazonSecretsManager client = new AmazonSecretsManagerClient("<aws-access-key-id>", "<aws-secret-access-key>", config);
+
+            GetSecretValueRequest request = new GetSecretValueRequest()
+            {
+                SecretId = secretName,
+                VersionStage = "AWSCURRENT" // VersionStage defaults to AWSCURRENT if unspecified.
+            };
+            GetSecretValueResponse response = null;
+            try
+            {
+                response = client.GetSecretValueAsync(request).Result;
+                cache = new SecretsManagerCache(client);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return response?.SecretString;
+        }
+        static void Main(string[] args)
+        {
+            var secretValue = GetSecret("<secret-name>");
+            var secretValueFromCache = Program.GetCacheData("<secret-name>");
+
+            Console.WriteLine("Secret fetched from AWS Secret Manager : " + secretValue);
+            Console.WriteLine("Secret fetched from AWS Cache Manager : " + secretValueFromCache);
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AWS.SecretManagerExample
+ How to implement AWS Secret Manager in .Net Console App
